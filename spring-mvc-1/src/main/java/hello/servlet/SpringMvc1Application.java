6d7fdf85233f48146032485cb2a1d1a59f6a443d
chosungil <csi@mlsoft.com>
2022-05-19 18:31:47 +0900

    git commit test

--- spring-mvc-1/src/main/java/hello/servlet/SpringMvc1Application.java
+++ spring-mvc-1/src/main/java/hello/servlet/SpringMvc1Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
-@ServletComponentScan // 하위 패키지에서 Servlet을 찾아 자동 등록
+@ServletComponentScan // 하위 패키지에서 Servlet 을 찾아 자동 등록
 @SpringBootApplication
 public class SpringMvc1Application {
 
