21e01df7bdd17d5ba782d7dfec9bb3eeba23ccae
chosungil <whtjddlf56@naver.com>
2022-08-02 23:43:48 +0900

    init project

---
+++ spring-mvc-1.1/src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,26 @@
+package hello.springmvc.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LogTestController {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @GetMapping("log-test")
+    public String logTest() {
+        String name = "Spring";
+
+        logger.trace("trace log = {}", name);
+        logger.debug("debug log = {}", name);
+        logger.info("info log = {}", name);
+        logger.warn("warn log = {}", name);
+        logger.error("error log = {}", name);
+
+        return "ok";
+    }
+}
