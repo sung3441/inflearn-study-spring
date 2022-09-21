6db53c0e171184413b2fdf6b1ff2d322fa886b27
chosungil <whtjddlf56@naver.com>
2022-09-22 01:31:26 +0900

    스프링 타입 컨버터

---
+++ 스프링_MVC_2편/spring-mvc-2.7-typeconverter/src/test/java/hello/typeconverter/formatter/FormattingConversionServiceTest.java
@@ -0,0 +1,33 @@
+package hello.typeconverter.formatter;
+
+import hello.typeconverter.converter.IpPortToStringConverter;
+import hello.typeconverter.converter.StringToIpPortConverter;
+import hello.typeconverter.type.IpPort;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.format.support.DefaultFormattingConversionService;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class FormattingConversionServiceTest {
+
+    @Test
+    void formattingConversionService() {
+        // converter 등록
+        DefaultFormattingConversionService conversionService = new DefaultFormattingConversionService();
+
+        conversionService.addConverter(new StringToIpPortConverter());
+        conversionService.addConverter(new IpPortToStringConverter());
+
+        // formatter 등록
+        conversionService.addFormatter(new MyNumberFormatter());
+
+        // converter 사용
+        IpPort ipPort = conversionService.convert("127.0.0.1:8080", IpPort.class);
+        assertThat(ipPort).isEqualTo(new IpPort("127.0.0.1", 8080));
+
+        // formatter 사용
+        assertThat(conversionService.convert(1000, String.class)).isEqualTo("1,000");
+        assertThat(conversionService.convert("1,000", Long.class)).isEqualTo(1000L);
+    }
+}
---
+++ 스프링_MVC_2편/spring-mvc-2.7-typeconverter/src/test/java/hello/typeconverter/formatter/MyNumberFormatterTest.java
@@ -0,0 +1,29 @@
+package hello.typeconverter.formatter;
+
+import net.bytebuddy.asm.Advice;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
+
+import java.text.ParseException;
+import java.util.Locale;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyNumberFormatterTest {
+
+    MyNumberFormatter formatter = new MyNumberFormatter();
+
+    @Test
+    void parse() throws ParseException {
+        Number result = formatter.parse("1,000", Locale.KOREA);
+        assertThat(result).isEqualTo(1000L); // Long 타입 주의
+    }
+
+    @Test
+    void print() {
+        String result = formatter.print(1000, Locale.KOREA);
+        assertThat(result).isEqualTo("1,000");
+    }
+}
