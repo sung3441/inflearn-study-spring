5eb0b54fd0c1789c76513d73a686d41861632e0f
chosungil <whtjddlf56@naver.com>
2023-03-09 14:04:54 +0900

    Add: 도메인 분석 설계

--- JPA_활용1/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ JPA_활용1/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -15,11 +15,12 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
+    @Column(name = "name")
     private String username;
 
     @Embedded
     private Address address;
 
-    @OneToMany(mappedBy = "member")
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
     private List<Order> orders = new ArrayList<>();
 }
--- JPA_활용1/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ JPA_활용1/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,21 +1,24 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.item.Item;
+import lombok.Setter;
 
 import javax.persistence.*;
 
-@Entity
+import static javax.persistence.FetchType.*;
+
+@Entity @Setter
 public class OrderItem {
 
     @Id @GeneratedValue
     @Column(name = "order_item_id")
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "item_id")
     private Item item;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "order_id")
     private Order order;
 
