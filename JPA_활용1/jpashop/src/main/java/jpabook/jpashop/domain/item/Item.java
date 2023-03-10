c7d3964595d3043125a4a672f73ebb27915363ef
chosungil <whtjddlf56@naver.com>
2023-03-10 23:33:05 +0900

    Add: 상품 도메인 개발

--- JPA_활용1/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ JPA_활용1/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -27,4 +28,26 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    // 비즈니스 로직, 엔티티가 해결할 수 있는 로직은 도메인 내에서 처리하는 것이 객체 지향적
+    // 데이터를 가지고 있는 곳에서 데이터를 처리하는 것이 응집력이 좋음
+    // setter로 데이터를 변경하는게 아니라 비즈니스 로직을 사용해서 변경
+
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
