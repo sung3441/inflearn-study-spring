c0b66a78d6610bd1f0bccc8df6ff96fad116e2fb
chosungil <whtjddlf56@naver.com>
2023-04-13 00:11:54 +0900

    Add: OSIV와 성능 최적화

---
+++ JPA_활용2/jpashop/src/main/java/jpabook/jpashop/service/query/OrderQueryService.java
@@ -0,0 +1,56 @@
+package jpabook.jpashop.service.query;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class OrderQueryService {
+
+    private final OrderRepository orderRepository;
+
+    public List<Order> ordersV1() {
+        List<Order> orders = orderRepository.findAllByCriteria(new OrderSearch());
+        for (Order order : orders) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.forEach(o -> o.getItem().getName());
+        }
+        return orders;
+    }
+
+    public List<OrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAllByCriteria(new OrderSearch());
+        return orders.stream()
+                .map(OrderDto::new)
+                .collect(toList());
+    }
+
+    public List<OrderDto> ordersV3() {
+        List<Order> orders = orderRepository.findAllWithItem();
+
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+        return result;
+    }
+
+    public List<OrderDto> ordersV3_page(int offset, int limit) {
+        return orderRepository.findAllWithMemberDelivery(offset, limit)
+                .stream()
+                .map(OrderDto::new)
+                .collect(toList());
+    }
+}
