e8f7534837c7e36cc45ce39e29f0b70ab620231c
chosungil <whtjddlf56@naver.com>
2023-04-30 22:59:49 +0900

    Add: Auditing, 도메인 클래스 컨버터, 페이징 & 정렬

---
+++ JPA_스프링_데이터/data-jpa/src/main/java/study/jpa/controller/MemberController.java
@@ -0,0 +1,45 @@
+package study.jpa.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import study.jpa.dto.MemberDto;
+import study.jpa.entity.Member;
+import study.jpa.repository.MemberRepository;
+
+import javax.annotation.PostConstruct;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@RestController
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/members/{id}")
+    public String findMember(@PathVariable("id") Long id) {
+        Member findMember = memberRepository.findById(id).get();
+        return findMember.getUsername();
+    }
+
+    @GetMapping("/members2/{id}")
+    public String findMember(@PathVariable("id") Member member) {
+        return member.getUsername();
+    }
+
+    @GetMapping("/members")
+    public Page<MemberDto> list(@PageableDefault(size = 5) Pageable pageable) {
+        return memberRepository.findAll(pageable).map(MemberDto::new);
+    }
+
+    @PostConstruct
+    public void init() {
+        for (int i = 1; i <= 100; i++) {
+            memberRepository.save(new Member("member" + i, i));
+        }
+    }
+}
--- JPA_스프링_데이터/data-jpa/src/test/java/study/jpa/entity/MemberTest.java
+++ JPA_스프링_데이터/data-jpa/src/test/java/study/jpa/entity/MemberTest.java
@@ -1,16 +1,19 @@
 package study.jpa.entity;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import study.jpa.repository.MemberRepository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
@@ -19,6 +22,9 @@ class MemberTest {
 
     @PersistenceContext
     EntityManager em;
+
+    @Autowired
+    MemberRepository memberRepository;
 
     @Test
     public void testEntity() {
@@ -51,5 +57,19 @@ class MemberTest {
             System.out.println("member = " + member);
             System.out.println("member.team = " + member.getTeam());
         }
+    }
+
+    @Test
+    public void JpaEventBaseEntity() throws Exception {
+        Member member = new Member("member1");
+        memberRepository.save(member); // @PrePersist
+
+        member.setUsername("member2");
+        em.flush(); // @PreUpdate
+        em.clear();
+
+        Member findMember = memberRepository.findById(member.getId()).get();
+
+        assertThat(findMember.getUsername()).isEqualTo("member2");
     }
 }
